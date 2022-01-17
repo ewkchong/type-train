b1f12de8f1bcfde81a98000732f2d38f57495185
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-01-16 21:24:43 -0800

    added some text-area logic + text-area grid

---
+++ static/app.js
@@ -0,0 +1,10 @@
+const exampleSentence = "This is an example sentence.";
+const sentenceArr = exampleSentence.split('');
+
+const textArea = document.querySelector(".text-area-grid");
+
+for (let i = 0; i < sentenceArr.length; i++) {
+    let letter = document.createElement("div");
+    letter.textContent = sentenceArr[i];
+    textArea.appendChild(letter);
+}
