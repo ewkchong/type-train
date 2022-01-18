67c3629b242fe769e357d82b3b229863465f50eb
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-01-18 02:33:35 -0800

    added restarting function

--- static/app.js
+++ static/app.js
@@ -1,6 +1,7 @@
 import WordGenerator from './WordGenerator.js';
 
-const textArea = document.querySelector(".text-area-container");
+let textArea = document.querySelector(".text-area-container");
+const textAreaDiv = document.querySelector(".text-area");
 
 let wordGen = new WordGenerator({});
 
@@ -21,5 +22,18 @@ async function populateTextField() {
 }
 
 populateTextField();
+
+document.addEventListener('keydown', (k) => {
+    if (k.code == "Escape") {
+        textArea.remove();
+        
+        const newTextArea = document.createElement("div");
+        newTextArea.classList = "text-area-container";
+        textAreaDiv.appendChild(newTextArea);
+        textArea = document.querySelector(".text-area-container");
+        populateTextField();
+        console.log("text field populated");
+    }
+})
 
 
