05eae2f7287c103ed6e7298e0214325e73958d93
Sam Zhao <samzhao273@gmail.com>
2022-01-20 19:25:46 -0800

    Added basic profile dropdown menu.

--- static/app.js
+++ static/app.js
@@ -73,4 +73,14 @@ refreshButton.addEventListener("mouseup", () => {
 })
 
 
+const profileIcon = document.getElementById("profile");
+const ddMenu = document.querySelector(".dd_menu");
+
+profileIcon.addEventListener("click", function() {
+    ddMenu.classList.toggle("active");
+})
+
+
+
+
 
