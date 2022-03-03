c9bd60cd6389c0231c2b0776afb46a23cb34d2e5
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-03-02 20:40:38 -0800

    added d3, results screen with graph

--- static/app.js
+++ static/app.js
@@ -14,7 +14,7 @@ const wordArray = [];
 const cursor = new Cursor(letterArray);
 const train = new Train();
 const stats = new Stats(Date.now());
-const timer = new Timer(30, train, stats);
+const timer = new Timer(15, train, stats);
 let allowedToType = true;
 
 document.onload = populateTextField();
