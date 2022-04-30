0fc98e9a6862c06f50d35766986b57d7be3ace92
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-04-29 18:44:47 -0700

    changing webpack config + changing files

--- static/app.js
+++ static/app.js
@@ -5,6 +5,37 @@ import * as Refresh from './modules/Refresh.js';
 import Train from './modules/Train.js';
 import Stats from './modules/Stats.js';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { connectAuthEmulator, getAuth, GoogleAuthProvider} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbEj3P2zx7TpYvnOcy1DaoEB9hRHFpHpU",
+  authDomain: "type-train.firebaseapp.com",
+  projectId: "type-train",
+  storageBucket: "type-train.appspot.com",
+  messagingSenderId: "380789326755",
+  appId: "1:380789326755:web:e465be129ab7a916071016",
+  measurementId: "G-ZR3VZRHSW9"
+};
+
+let debug = true;
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+if (debug) {
+  connectAuthEmulator(auth, 'http://localhost:9099');
+}
+
 let textArea = document.querySelector(".text-area-container");
 let wordGen = new WordGenerator({});
 let JSONready = false;
@@ -14,7 +45,7 @@ const wordArray = [];
 const cursor = new Cursor(letterArray);
 const train = new Train();
 const stats = new Stats(Date.now());
-const timer = new Timer(15, train, stats);
+const timer = new Timer(5, train, stats);
 let allowedToType = true;
 
 document.onload = populateTextField();
@@ -223,13 +254,6 @@ window.addEventListener('scroll', () => {
     cursor.updatePosition();
 }, true);
 
-
-
-
-
-
-
-
-
-
-
+document.addEventListener('timerend', () => {
+  console.log('timer ended');
+})
